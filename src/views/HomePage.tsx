e1df304caee91840ec44c21ab63226c5a2165c3e
max-loktionov <studreno@gmail.com>
2023-04-17 21:19:30 +0300

    feat: add gallery and styles

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,12 +1,16 @@
-import { Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
+import Gallery from "../components/Gallery";
 
 export default function HomePage() {
   return (
     <>
-      <Paper elevation={2}>
+      <Paper elevation={2} sx={{ height: "100vh" }}>
         <Typography variant="h4" textAlign="center" fontStyle="italic">
           Tweebric
         </Typography>
+        <Box display="flex" justifyContent={"center"} padding="64px">
+          <Gallery />
+        </Box>
       </Paper>
     </>
   );
